891b22b81a1b6394533288a8cf8868ba7b10c553
Oleh Kotyay <o.kotyay@ideil.com>
2021-05-28 09:05:44 +0300

    lead editor

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,13 +1,13 @@
-// @ckeditor/ckeditor5-indent-block
-
 import ClassicEditor from './classic';
 import BalloonEditor from './balloon';
 import InlineEditor from './inline';
 import SimpleEditor from './simple';
+import LeadEditor from './lead';
 
 export default {
     ClassicEditor,
     BalloonEditor,
     InlineEditor,
-    SimpleEditor
+    SimpleEditor,
+    LeadEditor
 };
